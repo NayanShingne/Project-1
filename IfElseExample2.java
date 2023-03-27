68b4e7b92c38829a3aeb359e7b00da30c1c23548
NayanShingne <129051343+NayanShingne@users.noreply.github.com>
2023-03-27 17:29:25 +0530

    if else uploaded

---
+++ IfElseExample2.java
@@ -0,0 +1,19 @@
+package day3;
+
+public class IfElseExample2 {
+
+	public static void main(String[] args) {
+		
+		 int y=10;
+		 if(y==10)
+			System.out.println("welcome");
+		 else if(y>5)
+			 System.out.println("hello");
+		 else if (y<20)
+			 System.out.println("testing");
+		 else 
+			System.out.println("error");
+
+	}
+
+}
